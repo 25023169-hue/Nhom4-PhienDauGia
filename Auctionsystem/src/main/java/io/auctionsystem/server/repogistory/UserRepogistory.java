86f5700db010a952b4135b64d5f3ef79b114ce05
Haian-12 <annguyen702101@gmail.com>
2026-05-01 16:09:13 +0700

    .

--- Auctionsystem/src/main/java/io/auctionsystem/server/repogistory/UserRepogistory.java
+++ Auctionsystem/src/main/java/io/auctionsystem/server/repogistory/UserRepogistory.java
@@ -2,7 +2,11 @@ package io.auctionsystem.server.repogistory;
 
 import io.auctionsystem.server.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -14,4 +18,8 @@ public interface UserRepogistory extends JpaRepository<User, Long> {
 
     // Kiểm tra xem username đã tồn tại chưa (phục vụ chức năng Đăng ký)
     boolean existsByUsername(String username);
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query(value = "INSERT INTO sellers (id, store_name, rating) VALUES (:userId, :storeName, 0.0)", nativeQuery = true)
+    void upgradeToSeller(@Param("userId") Long userId, @Param("storeName") String storeName);
 }
--- Auctionsystem/src/main/java/io/auctionsystem/server/service/AuthService.java
+++ Auctionsystem/src/main/java/io/auctionsystem/server/service/AuthService.java
@@ -14,8 +14,6 @@ public class AuthService {
 
     @Autowired
     private UserRepogistory userRepogistory;
-    @Autowired
-    private UserRepogistory userRepo;
 
     public String register(RegisterRequest request) {
         if (userRepogistory.existsByUsername(request.getUsername())) {
