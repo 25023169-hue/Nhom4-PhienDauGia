47fa2886456479c341bdc3a67520ab0651391e77
Mai Đức Anh <maiducanhht2000@gmail.com>
2026-05-21 23:28:46 +0700

    feat: Thêm model Fashion, Jewelry, Anti-sniping AOP và seed 9 sản phẩm test

--- src/main/java/io/auctionsystem/common/enums/ItemType.java
+++ src/main/java/io/auctionsystem/common/enums/ItemType.java
@@ -3,5 +3,7 @@ package io.auctionsystem.common.enums;
 public enum ItemType {
     ART,
     ELECTRONICS,
-    VEHICLE
+    VEHICLE,
+    FASHION,
+    JEWELRY
 }
---
+++ src/main/java/io/auctionsystem/server/config/AntiSnipingAspect.java
@@ -0,0 +1,28 @@
+package io.auctionsystem.server.config;
+
+import io.auctionsystem.common.response.BidResponse;
+import io.auctionsystem.server.service.AntiSnipingService;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AntiSnipingAspect {
+
+    @Autowired
+    private AntiSnipingService antiSnipingService;
+
+    // Kích hoạt ngay sau khi hàm placeBid của BidService chạy xong và trả về kết quả
+    @AfterReturning(pointcut = "execution(* io.auctionsystem.server.service.BidService.placeBid(..))", returning = "result")
+    public void afterBidPlacedCheckSniping(Object result) {
+        if (result instanceof BidResponse) {
+            BidResponse response = (BidResponse) result;
+            if (response.isAccepted()) {
+                // Kiểm tra và thông báo gia hạn thời gian hoàn toàn độc lập với BidService
+                antiSnipingService.notifyIfAntiSnipingTriggered(response.getAuctionId());
+            }
+        }
+    }
+}
