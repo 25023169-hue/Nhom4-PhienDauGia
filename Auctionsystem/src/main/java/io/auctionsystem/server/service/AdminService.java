97d6ceed7bd0a01eb6d038693b2a232af94dc2b2
DELL <ducanh23122006@gmail.com>
2026-05-26 22:30:28 +0700

    admin

---
+++ Auctionsystem/src/main/java/io/auctionsystem/server/service/AdminService.java
@@ -0,0 +1,43 @@
+package io.auctionsystem.server.service;
+
+import io.auctionsystem.server.model.User;
+import io.auctionsystem.server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // 1. Hàm lấy toàn bộ danh sách User (Phải trùng tên với bên Controller gọi)
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // 2. Hàm Khóa/Mở khóa tài khoản
+    public boolean toggleBanUser(Long id) {
+        Optional<User> userOpt = userRepository.findById(id);
+        if (userOpt.isPresent()) {
+            User user = userOpt.get();
+            // Đảo ngược trạng thái ban (nếu đang true thì thành false và ngược lại)
+            // Lưu ý: Trong Model User của bạn phải có biến boolean isBanned hoặc tương tự
+            user.setBanned(!user.isBanned());
+            userRepository.save(user);
+            return true;
+        }
+        return false;
+    }
+
+    // 3. Hàm xóa User
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+    public boolean isSeller(Long userId) {
+        return userRepository.isUserSeller(userId) > 0;
+    }
+}
